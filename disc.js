fd4542d4a4bf2f2e852fb820321dd23cb7c939d7
magnumbaguette <blue01claws@gmail.com>
2018-11-16 21:53:15 +0530

    First commit

---
+++ disc.js
@@ -0,0 +1,29 @@
+/**
+ * A ping pong boredacted
+ */
+
+// Imporedacted
+redacted
+
+// Credacted
+redacted
+
+/**
+ * Thredacted
+ * redacted
+ */
+redacted
+  redacted
+});
+
+// Credacted
+redacted
+  // If redacted
+  if (mredacted
+    // Sredacted
+    mredacted
+  }
+});
+
+// Log ouredacted
+redacted
